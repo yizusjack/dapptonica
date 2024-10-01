369c93d2ed1ab3721b9e3dd0e558ba8ea7914b56
Jesús Pérez Hernández <yizussawyer@gmail.com>
2024-09-30 23:12:12 -0600

    WIP: signup

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,14 +2,24 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\SignupRequest;
 
 class UserController extends Controller
 {
-    public function signup(Request $request) {
-        return response()->json([
-            $request->name,
+    public function signup(SignupRequest $request) {
+        $user = $request->validated();
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
         ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response(compact('user', 'token'));
     }
 }
--- dapp/src/views/InicioSesion/Signup.jsx
+++ dapp/src/views/InicioSesion/Signup.jsx
@@ -1,11 +1,12 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import MKBox from '../../components/template/MKBox'
 import LoginLayout from '../../components/LoginLayout'
 import MKInput from '../../components/template/MKInput'
 import MKTypography from '../../components/template/MKTypography'
 import MKButton from '../../components/template/MKButton'
-import { Link } from 'react-router-dom'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
 import axiosClient from '../../axios-client'
+import { useStateContext } from '../../contexts/ContextProvider'
 
 export default function Signup() {
 
@@ -15,11 +16,10 @@ export default function Signup() {
   const passwordRef = useRef(null);
   const passwordConfirmationRef = useRef(null);
 
-  const [name, setName] = useState('');
+  const [errors, setErrors] = useState([]);
+  const {user, setUser, setToken} = useStateContext(); //agarra lo que se definio en el contexto
+  const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    setName(e.target.value);
-  };
 
   const guardar = (ev) => {
     ev.preventDefault()
@@ -31,16 +31,20 @@ export default function Signup() {
       password_confirmation: passwordConfirmationRef.current.value,
     }
 
-    console.log(request);
+    /*console.log(request);*/
 
     axiosClient.post('/signup', request)
     .then(({data}) => { //las llaves son para solo acceder al data, podria tener otras cosas tambien
-      console.log(data)
+      setUser(data.user)
+      setToken(data.token)
+      console.log("Who tf do you think I am?");
+      useEffect(() => {
+          navigate("/about-us");
+      }, [navigate]);
     })
     .catch(err => {
       const response = err.response;
       if (response && response.status === 422) { //error de validacion
-        console.log(response.data.errors);
         setErrors(response.data.errors);
       }
     })
@@ -50,24 +54,42 @@ export default function Signup() {
   return (
     <LoginLayout>
       <MKBox component="form" role="form" onSubmit={guardar}>
-        <MKBox mb={2}>
-          <MKInput inputRef={nameRef} type="text" label="Nombre" fullWidth onChange={handleChange} />
+        <MKBox mb={2} fontWeight="light">
+          <MKInput inputRef={nameRef} type="text" label="Nombre" error={"name" in errors ? true : false} fullWidth />
+          {
+            "name" in errors &&
+            <MKTypography color="error" variant="caption" fontWeight="regular">
+              {errors.name[0]}
+            </MKTypography>
+          }
         </MKBox>
         <MKBox mb={2}>
-          <MKInput inputRef={emailRef} type="email" label="Correo electrónico" fullWidth/>
+          <MKInput inputRef={emailRef} type="email" label="Correo electrónico" required="true" error={"email" in errors ? true : false} fullWidth/>
+          {
+            "email" in errors &&
+            <MKTypography color="error" variant="caption" fontWeight="regular">
+              {errors.email[0]}
+            </MKTypography>
+          }
         </MKBox>
         <MKBox mb={2}>
-          <MKInput inputRef={passwordRef} type="password" label="Contraseña" fullWidth />
+          <MKInput inputRef={passwordRef} type="password" label="Contraseña" required="true" error={"password" in errors ? true : false} fullWidth />
+          {
+            "password" in errors &&
+            <MKTypography color="error" variant="caption" fontWeight="regular">
+              {errors.password[0]}
+            </MKTypography>
+          }
         </MKBox>
         <MKBox mb={2}>
-          <MKInput inputRef={passwordConfirmationRef} type="password" label="Confirmar Contraseña" fullWidth />
+          <MKInput inputRef={passwordConfirmationRef} type="password" label="Confirmar Contraseña" required="true" fullWidth />
         </MKBox>
         <MKBox mt={4} mb={1}>
           <MKButton variant="gradient" color="info" type="submit" fullWidth>
             Registrarse
           </MKButton>
         </MKBox>
-        <MKBox mt={3} mb={1} textAlign="center">
+        <MKBox mt={3} mb={1} textAlign="center" fontWeight="light">
           <MKTypography variant="button" color="text">
             ¿Ya tienes una cuenta? {" "}
             <MKTypography
